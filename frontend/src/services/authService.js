7b5a24cbbe86781c37e895eef6db1f55b359a2fe
Xiu <62536245+xiuxiuli@users.noreply.github.com>
2025-04-10 01:28:36 -0700

    triiger

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -7,7 +7,7 @@ const authService = {
   getProfile: async () => {
     try {
       const res = await httpService.get("/auth/profile");
-      console.log("✅ /profle 返回用户数据：", res); // ✅ 加这行
+      console.log("✅ /profle 返回用户数据：", res); // 
       return res;
     } catch (error) {
       console.error("❌ /profile 请求失败：", error);
